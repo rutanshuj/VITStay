1be1a9bd2a63efd6bdce7639e6106e674a58cb92
rutanshuj <rjhaveri41@gmail.com>
2018-01-25 20:43:01 +0530

    index route created

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-const http = require('http');
+const http = require('http'); //Core Module
 const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path'); //Core Module
 
+const app = express();
 const hostname = '127.0.0.1';
 const port = 3000;
 
-fs.readFile('index.html', (err, html) =>{
-    if(err){
-        throw err;
-    }
-    //Creates a server
-    const server = http.createServer((req, res) => {
-        res.statusCode=200;
-        res.setHeader('Content-type', 'text/html');
-        res.write(html);
-        res.end();
-    });
+/* const logger = (req, res, next) =>{
+    console.log('Logging...');
+    next(); //next to fire next middleware and to end this
+}
+app.use(logger); //logger is the middleware required and app.use uses it
+*/
 
-    server.listen(port, hostname, () => {
-        console.log('Server Started on port:' + port);
-    });
+//Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Set static path
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) =>{
+   res.send("Hello World") ;
+});
+
+
+
+app.listen(port, ()=> {
+   console.log('Server Started on Port:'+port);
 });
